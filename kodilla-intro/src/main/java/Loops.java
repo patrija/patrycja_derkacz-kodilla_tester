25be0166d8c92fa1bfdfa5e216d36a67830a466e
Patrycja Derkacz <ap.derkacz@gmail.com>
2020-01-24 22:29:48 +0100

    Loops

---
+++ kodilla-intro/src/main/java/Loops.java
@@ -0,0 +1,13 @@
+public class Loops {
+
+    public int getSum(int[] num) {
+        int sum = 0;
+
+        for (int i = 0; i < num.length; i++) {
+            sum = sum + num[i];
+        }
+        return sum;
+    }
+
+
+}
---
+++ kodilla-intro/src/main/java/MainLoops.java
@@ -0,0 +1,14 @@
+public class MainLoops {
+
+    public static void main(String[] args) {
+
+        Loops Tablica = new Loops();
+
+        int[] numbers = new int[] {2, 4, 5};
+
+        int wynik = Tablica.getSum(numbers);
+        System.out.println(wynik);
+
+    }
+}
+
