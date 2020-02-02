340514458dce29ef2bd35598143789908d39902a
Patrycja Derkacz <ap.derkacz@gmail.com>
2020-02-02 11:27:41 +0100

    Task Petle

---
+++ kodilla-collections/src/main/java/com/kodilla/collections/lists/homework/CarsListApplication.java
@@ -0,0 +1,31 @@
+package com.kodilla.collections.lists.homework;
+
+import com.kodilla.collections.arrays.homework.CarUtils;
+import com.kodilla.collections.interfaces.homework.Car;
+import com.kodilla.collections.interfaces.homework.Honda;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarsListApplication {
+    public static void main(String[] args) {
+        List<Honda> cars = new ArrayList<>();
+        cars.add(new Honda(40, 20, 100));
+        cars.add(new Honda(50, 30, 80));
+        Honda honda3 = new Honda(20,10,50);
+        cars.add(honda3);
+
+        for(Honda honda : cars) {
+            CarUtils.describeCar(honda);
+        }
+        System.out.println("Po usunieciu");
+        cars.remove(0);
+        cars.remove(honda3);
+
+        for(Honda honda : cars) {
+            CarUtils.describeCar(honda);
+        }
+
+        System.out.println(cars.size());
+    }
+}
