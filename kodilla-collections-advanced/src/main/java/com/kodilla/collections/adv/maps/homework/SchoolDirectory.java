e6d5d130c57bebedc6dde754fc9d8b12621afc1f
Patrycja Derkacz <ap.derkacz@gmail.com>
2020-02-08 22:52:22 +0100

    Task 4.3 Kolekcje- listy

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/SchoolDirectory.java
@@ -0,0 +1,30 @@
+package com.kodilla.collections.adv.maps.homework;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SchoolDirectory {
+    public static void main(String[] args) {
+        Map<Principal, School> szkola = new HashMap<>();
+
+        Principal kazimierz = new Principal("Kazimierz", "W");
+        Principal jolanta = new Principal("Jolanta", "G");
+        Principal natalia   = new Principal("Natalia", "M");
+
+        School kazimierzSchool = new School("HighSchoolNo1",10, 15, 16);
+        School jolantaSchool = new School("HighSchoolNo5",30, 35, 28, 27);
+        School nataliaSchool = new School("HighSchoolNo2",12,20,15,28,19);
+
+        szkola.put(kazimierz, kazimierzSchool);
+        szkola.put(jolanta, jolantaSchool);
+        szkola.put(natalia, nataliaSchool);
+
+        for(Map.Entry<Principal, School> principalEntry : szkola.entrySet())
+        System.out.println(principalEntry.getKey().getFirstname() + " " + principalEntry.getKey().getLastname()+ " is Principal of " + principalEntry.getValue().getSchoolName() + ". Total numbers of students is  " + principalEntry.getValue().getSum());
+    }
+
+
+
+}
