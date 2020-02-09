bc6c96022ee27e86ab3d2dc55950625e109df805
Patrycja Derkacz <ap.derkacz@gmail.com>
2020-02-09 10:29:41 +0100

    Task 5.2

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/School.java
@@ -0,0 +1,38 @@
+package com.kodilla.collections.adv.maps.homework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class School {
+    private List<Double> school = new ArrayList<>();
+
+    private String schoolName;
+
+    public School(String schoolName, double... school) {
+        this.schoolName = schoolName;
+        for(double number : school)
+            this.school.add(number);
+    }
+
+    public double getSum() {
+        double licznik = 0;
+        for(double num : school)
+            licznik = licznik + num;
+        return licznik;
+    }
+
+    @Override
+    public String toString() {
+        return "School{" +
+                "schoolName='" + schoolName + '\'' +
+                '}';
+    }
+
+    public List<Double> getSchool() {
+        return school;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+}
