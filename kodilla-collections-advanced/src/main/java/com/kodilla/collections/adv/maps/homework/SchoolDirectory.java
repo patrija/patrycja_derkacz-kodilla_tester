a45f985e95aee45f76f02e6eacd1901ba16dc061
Patrycja Derkacz <ap.derkacz@gmail.com>
2020-02-09 09:37:41 +0100

    Task 5.2 szkoly i dyrektorzy

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/SchoolDirectory.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/SchoolDirectory.java
@@ -11,15 +11,18 @@ public class SchoolDirectory {
 
         Principal kazimierz = new Principal("Kazimierz", "W");
         Principal jolanta = new Principal("Jolanta", "G");
-        Principal natalia   = new Principal("Natalia", "M");
+        Principal natalia = new Principal("Natalia", "M");
+        Principal grazyna = new Principal("Grazyna", "G");
 
         School kazimierzSchool = new School("HighSchoolNo1",10, 15, 16);
         School jolantaSchool = new School("HighSchoolNo5",30, 35, 28, 27);
         School nataliaSchool = new School("HighSchoolNo2",12,20,15,28,19);
+        School grazynaSchool = new School("HighSchoolNo8", 14,18,15,13,250);
 
         szkola.put(kazimierz, kazimierzSchool);
         szkola.put(jolanta, jolantaSchool);
         szkola.put(natalia, nataliaSchool);
+        szkola.put(grazyna, grazynaSchool);
 
         for(Map.Entry<Principal, School> principalEntry : szkola.entrySet())
         System.out.println(principalEntry.getKey().getFirstname() + " " + principalEntry.getKey().getLastname()+ " is Principal of " + principalEntry.getValue().getSchoolName() + ". Total numbers of students is  " + principalEntry.getValue().getSum());
