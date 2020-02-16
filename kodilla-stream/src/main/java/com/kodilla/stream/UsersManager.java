b437d503c749cde593b92948665f503b29fa13ea
Patrycja Derkacz <ap.derkacz@gmail.com>
2020-02-16 14:06:12 +0100

    Stream exceptions

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/UsersManager.java
@@ -0,0 +1,19 @@
+package com.kodilla.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UsersManager {
+    public static void main(String[] args) {
+        List<String> usernames= UsersRepository.getUserList()
+                .stream()
+                .filter(u ->u.getGroup().equals("Chemist") )
+                .map(UsersManager::getUserName)
+                .collect(Collectors.toList());
+                 System.out.println(usernames);
+    }
+
+    public static String getUserName(User user){
+        return user.getUsername();
+    }
+}
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/exception/FirstExample.java
@@ -0,0 +1,12 @@
+package com.kodilla.stream.exception;
+
+import java.sql.SQLOutput;
+
+public class FirstExample {
+    public static void main(String[] args) {
+        String username = "John";
+        String[] nameParts = username.split(" ");
+        System.out.println("First name: " + nameParts[0]);
+        System.out.println("Last name: " + nameParts[1]);
+    }
+}
