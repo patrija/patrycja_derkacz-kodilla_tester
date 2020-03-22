3d6fdeaf0adb1da67e0933ffae66b2268e20d438
Patrycja Derkacz <ap.derkacz@gmail.com>
2020-03-22 15:44:08 +0100

    Task 9.1 Eliminowanie zaleznosci POPRAWA

---
+++ kodilla-spring-basics/src/main/java/com/kodilla/spring/basic/dependency_injection/homework/Notification.java
@@ -0,0 +1,8 @@
+package com.kodilla.spring.basic.dependency_injection.homework;
+
+public interface Notification {
+
+     void success(String address);
+
+     void fail(String address);
+}
--- kodilla-spring-basics/src/main/java/com/kodilla/spring/basic/dependency_injection/homework/NotificationService.java
+++ kodilla-spring-basics/src/main/java/com/kodilla/spring/basic/dependency_injection/homework/NotificationService.java
@@ -1,7 +1,6 @@
 package com.kodilla.spring.basic.dependency_injection.homework;
 
-public class NotificationService {
-
+public class NotificationService implements Notification{
     public void success(String address) {
         System.out.println("Package delivered to: " + address);
     }
