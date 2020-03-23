ebd25591b48555cac2d93e06bb9f1c5b02600196
Patrycja Derkacz <ap.derkacz@gmail.com>
2020-03-23 11:44:17 +0100

    Task 9.3 Wstrzykiwanie beanow

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework2/DeliveryService.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework2/DeliveryService.java
@@ -1,13 +1,14 @@
-package com.kodilla.spring.basic.dependency_injection.homework;
+package com.kodilla.spring.basic.spring_dependency_injection.homework2;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class DeliveryService {
 
     public boolean deliverPackage(String address, double weight) {
         if (weight > 30) {
-            System.out.println("Package too heavy");
             return false;
         }
-        System.out.println("Delivering in progress...");
         return true;
     }
 }
