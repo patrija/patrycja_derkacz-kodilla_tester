e8667190a80eae48585be93509dfe6156a64077d
Patrycja Derkacz <ap.derkacz@gmail.com>
2020-04-09 18:24:28 +0200

    Task 6.3 Strumienie typow liczobwych poprawa

--- kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
@@ -6,9 +6,8 @@ public class ForumStats {
     public static void main(String[] args) {
        double avg = UsersRepository.getUserList()
                 .stream()
-               .filter(a->a.getAge()>=40)
-                .map(u->u.getNumberOfPosts())
-                .mapToInt(n->n)
+                .filter(a->a.getAge() >= 40)
+                .mapToInt(n->n.getNumberOfPosts())
                 .average()
                .getAsDouble();
         //System.out.println(avg);
@@ -16,8 +15,7 @@ public class ForumStats {
        double avg2  = UsersRepository.getUserList()
                .stream()
                .filter(a->a.getAge() < 40)
-               .map(u->u.getNumberOfPosts())
-               .mapToInt(n->n)
+               .mapToInt(u->u.getNumberOfPosts())
                .average()
                .getAsDouble();
         //System.out.println(avg2);
